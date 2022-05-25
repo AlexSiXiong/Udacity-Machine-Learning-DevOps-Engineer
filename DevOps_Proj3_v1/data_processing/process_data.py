9f2d8a7d075e7a9ec084e60aa2db50157d379b35
Ubuntu <ubuntu@ip-10-0-31-229.ap-southeast-2.compute.internal>
2022-05-25 23:18:47 +0000

    modify tests

--- DevOps_Proj3_v1/data_processing/process_data.py
+++ DevOps_Proj3_v1/data_processing/process_data.py
@@ -69,8 +69,12 @@ def save_encoder_classes(path, attribute, le):
 def label_encoder(df, attribute, training):
     le = preprocessing.LabelEncoder()
     path = data_folder_path("DevOps_Proj3_v1", 'data/label_encoders', f'{attribute}.npy')
+
     if not os.path.exists(path):
         path = data_folder_path("Udacity-Machine-Learning-DevOps-Engineer/DevOps_Proj3_v1", 'data/label_encoders', f'{attribute}.npy')
+    if not os.path.exists(path):
+        path = data_folder_path("data", 'label_encoders', f'{attribute}.npy')
+        
     if training:
         le.fit(list(set(df[attribute])))        
         save_encoder_classes(path, attribute, le)
@@ -93,6 +97,10 @@ def onehot_encoder(df, attribute, training):
     if not os.path.exists(path):
 
         path = data_folder_path("Udacity-Machine-Learning-DevOps-Engineer/DevOps_Proj3_v1", 'data/onehot_encoders', f'{attribute}.npy')
+        
+    if not os.path.exists(path):
+        path = data_folder_path("data", 'label_encoders', f'{attribute}.npy')
+        
     if training:
         lb.fit(list(set(df[attribute])))
         save_encoder_classes(path, attribute, lb)    
